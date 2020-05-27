70cb3defeb0e9943cdd3fa529d6967abcdbe93bd
JSchrtke <johannes.schroetke@gmail.com>
2020-05-27 21:49:20 +0200

    Improve fcd speed in large dirs
    Requires fd and fzf

--- fcd.ps1
+++ fcd.ps1
@@ -1,6 +1,7 @@
 if ($args.Count -eq 0) {
-    Invoke-FuzzySetLocation $PWD
+    $(Resolve-Path ~).Path | cd && fd | & fzf --height 50% | cd
 }
 else {
-    Invoke-FuzzySetLocation $args
+    $path = Resolve-Path($args[0])
+    $path | cd && fd | & fzf --height 50% | cd
 }
