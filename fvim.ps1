f02d849d38bc342598eb15b2a2cf5d50a77da31c
JSchrtke <johannes.schroetke@gmail.com>
2020-05-28 23:12:31 +0200

    Fix fvim always opening in home directory

--- fvim.ps1
+++ fvim.ps1
@@ -6,7 +6,7 @@ else {
 }
 
 $CurrentPath = ($PWD).Path
-$FilePath = $(Resolve-Path ~).Path | cd && & fzf --height 50% --preview 'bat --style=numbers --color=always {} | head -500'
+$FilePath = $(Resolve-Path $UserPath).Path | cd && & fzf --height 50% --preview 'bat --style=numbers --color=always {} | head -500'
 if ($FilePath) {
     vim $FilePath
 }
