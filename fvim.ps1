dbeaf1ed1b095576f3a4718479ee800f30acf060
JSchrtke <johannes.schroetke@gmail.com>
2020-05-28 12:09:18 +0200

    Fix fvim opening vim when not selecting anything

--- fvim.ps1
+++ fvim.ps1
@@ -1,12 +1,16 @@
 if ($args.Count -eq 0) {
     $CurrentPath = ($PWD).Path
     $FilePath = $(Resolve-Path ~).Path | cd && & fzf --height 50% --preview 'bat --style=numbers --color=always {} | head -500'
-    vim $FilePath
+    if ($FilePath) {
+        vim $FilePath
+    }
     $CurrentPath | cd
 }
 else {
     $CurrentPath = ($PWD).Path
-    $FilePath = $(Resolve-Path $args[0]).Path | cd && & fzf --height 50%
-    vim $FilePath
+    $FilePath = $(Resolve-Path $args[0]).Path | cd && & fzf --height 50% --preview 'bat --style=numbers --color=always {} | head -500'
+    if ($FilePath) {
+        vim $FilePath
+    }
     $CurrentPath | cd
 }
