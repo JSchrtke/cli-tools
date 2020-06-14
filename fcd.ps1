ddef89ee603b548986b4524e8b9c45ef50b83050
JSchrtke <johannes.schroetke@gmail.com>
2020-06-14 14:20:24 +0200

    Fix error when passing invalid path parameter

--- fcd.ps1
+++ fcd.ps1
@@ -1,8 +1,15 @@
 if ($args.Count -eq 0) {
-    $path = $(Resolve-Path ~).Path
+    $path = "~"
 }
 else {
-    $path = Resolve-Path($args[0])
+    $path = $args[0]
+}
+Try {
+    $destination = Resolve-Path($path) -ErrorAction Stop
+}
+Catch {
+    Write-Output $(($PWD).Path + "\" + $path + " does not exist...")
+    Write-Output "Using home directory instead"
 }
 
 $destination | Set-Location && fd | fzf --height 50% | Set-Location
