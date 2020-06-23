9da9487a5eeb9b2f0d4abaac2049234efab877bc
JSchrtke <johannes.schroetke@eddymax.com>
2020-06-23 12:32:24 +0200

    Add fuzzy open shorthand

---
+++ fo.ps1
@@ -0,0 +1,27 @@
+if ($args.Count -eq 0) {
+    $path = "~"
+}
+else {
+    $path = $args[0]
+}
+Try {
+    $startingDir = Resolve-Path($path) -ErrorAction Stop
+}
+Catch {
+    Write-Output $(($PWD).Path + "\" + $path + " does not exist...")
+    Write-Output "Using home directory instead"
+}
+
+$originalDir = ($PWD).Path
+
+$fzfOutput = $($startingDir | Set-Location && fd | fzf --height 50% --preview 'bat --style=numbers --theme=ansi-dark --color=always {} | head -500')
+if ($fzfOutput) {
+    if (($fzfOutput | Test-Path -PathType Container)) {
+        explorer $fzfOutput
+    }
+    else {
+        code $fzfOutput
+    }
+}
+
+$originalDir | Set-Location
