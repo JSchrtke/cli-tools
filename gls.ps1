12df979b74d1fd294fa7c2118f83894fa965714c
JSchrtke <johannes.schroetke@gmail.com>
2020-08-10 19:33:46 +0200

    Update git log shorthand

--- gls.ps1
+++ gls.ps1
@@ -1,1 +1,1 @@
-git log --graph --oneline --pretty=format:"%C(Yellow)%h%Creset %C(auto)%d%Creset %s %C(Magenta)(%an, %cr)%Creset" $args
+git log --graph --oneline --pretty=format:"%C(Yellow)%h%Creset %C(auto)%d%Creset %s %C(Green)(%an, %cr)%Creset" $args
