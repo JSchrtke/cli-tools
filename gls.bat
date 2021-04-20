a44ca39278eddaf62bc0ef52f326fc4f29566f30
JSchrtke <jschrtke@protonmail.com>
2021-04-20 16:22:29 +0200

    Fix wrong format string

--- gls.bat
+++ gls.bat
@@ -1,1 +1,1 @@
-git log --graph --oneline --pretty=format:"%C(Yellow)%h%Creset %C(auto)%d%Creset %s %C(Green)(%an, %cr)%Creset" %*
+git log --graph --oneline --pretty=format:"%%C(Yellow)%%h%%Creset %%C(auto)%%d%%Creset %%s %%C(Green)(%%an, %%cr)%%Creset"
